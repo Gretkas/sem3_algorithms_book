6aeff385379f125b4b0861d4eaa87ee912711a77
Sergio Martinez <sergimar@stud.ntnu.no>
2020-08-23 20:45:32 +0200

    fixing exponential method

--- src/main/java/chapterTwo/Subset.java
+++ src/main/java/chapterTwo/Subset.java
@@ -2,30 +2,39 @@ package chapterTwo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Subset {
-    // cubic time complexity??
-    public ArrayList<ArrayList<Integer>> subset(int[] set) {
-        ArrayList<ArrayList<Integer>> subsets = new ArrayList<>() ;
-        for (int k = set.length; k > 0; k--) {
-            set = Arrays.copyOfRange(set, 0, k);
-            for (int i = 0; i < set.length; i++) {
-                ArrayList<Integer> subset = new ArrayList<>();
-                for (int j = i; j < set.length; j++) {
-                    subset.add(set[j]);
-                }
-                subsets.add(subset);
-            }
+
+
+    /**
+     * return all subsets of a set exponential time
+     *
+     * @param set the set
+     * @return the array list
+     */
+    public ArrayList<ArrayList<Integer>> subsetRec(int[] set) {
+        if (set.length == 0) {
+            ArrayList<ArrayList<Integer>> x = new ArrayList<>();
+            x.add(new ArrayList<>());
+            return x;
         }
-        subsets.add(new ArrayList<>());
+        ArrayList<ArrayList<Integer>> subsets = new ArrayList<>(subsetRec(Arrays.copyOfRange(set, 0, set.length-1)));
+        subsets.addAll(subsetRec(Arrays.copyOfRange(set, 0, set.length-1)));
+
+        for (int i = (int) Math.ceil((double) subsets.size() / 2); i < subsets.size(); i++) {
+            subsets.get(i).add(set.length-1);
+        }
+
         return subsets;
     }
 
     public static void main(String[] args) {
         Subset subset = new Subset();
-        int[] set = {0, 1, 2, 3, 4, 5};
-        for (int i = 0; i < subset.subset(set).size(); i++) {
-            System.out.println(subset.subset(set).get(i).toString());
+        int[] set = {0, 1, 2, 3};
+        for (int i = 0; i < subset.subsetRec(set).size(); i++) {
+            System.out.println(subset.subsetRec(set).get(i).toString());
         }
     }
 }
